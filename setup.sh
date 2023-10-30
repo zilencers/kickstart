c86b20dfdcae9008d525e453605e85f03722e150
zilencer <zilencer@protonmail.com>
2023-10-30 11:36:51 -0500

    ks.cfg will now get a unique filename

--- setup.sh
+++ setup.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-# TODO: Append timestamp
 LOG="/var/log/kickstart.log"
 SYSTEMD_NET_PATH="/etc/systemd/network/"
 
 help() {
-   echo "TODO: Add help"
+   echo "Usage: setup.sh [OPTIONS]"
+   echo ""
+   echo "-d |--disk	   disk the OS will be installed on"
+   echo "    --dns         IP Address of the DNS to use"
+   echo "-i |--ip-range    pool of IP addresses to use for container network."
+   echo "-if|--interface   interface that will be used for macvlan or ipvlan"
+   echo "-ip|--ip-address  static IP address to assign to a container"
+   echo "-g |--gateway     gateway IP to use for container network"
+   echo "-p |--packages    comma separated list of packages to be installed"
+   echo "-s |--subnet      subnet for the container network"
+   echo "-h |--help        print help"
    exit 1
 }
 
@@ -80,7 +89,7 @@ required_pkg_check() {
       dnf install -y -q podman
    fi
 
-   printf "Done\n"
+   printf "Done\n" | tee $LOG
 }
 
 update_config() {
@@ -88,72 +97,77 @@ update_config() {
    
    sed -i "s/???/$DISK/g" config/ks.cfg
    
-   printf "Done\n"
+   printf "Done\n" | tee $LOG
 }
 
 setup_container_net() {
-   printf "Creating podman network ... "
+   printf "Creating podman network ... " | tee $LOG
 
    if [[ ! $(podman network ls | grep -i kickstart) ]]; then
       podman network create \
-	      -d macvlan \
+	      -d ipvlan \
 	      --subnet $SUBNET \
 	      --gateway $GATEWAY \
               --ip-range $IP \
 	      -o parent=$INTERFACE \
 	      kickstart
    else
-      printf "exists, skipping\n"
+      printf "exists, skipping\n" | tee $LOG
    fi
 }
 
 create_image() {
-   printf "Creating podman image ...\n"
+   printf "Creating podman image ...\n" | tee $LOG
 
    if [[ ! $(podman images | grep -o kickstart) ]]; then
 
       podman build -q -t kickstart -f config/Dockerfile
 
-      printf "Done\n"
+      printf "Done\n" | tee $LOG
    else
-      printf "exists, skipping\n"
+      printf "exists, skipping\n" | tee $LOG
    fi
 }
 
 create_container() {
-   printf "Creating podman container ...\n"
+   printf "Creating podman container ...\n" | tee $LOG
 
    if [[ ! $(podman ps -a | grep -o kickstart) ]]; then  
    
       podman run -d -q \
-	      --ip 192.168.0.200 \
+	      --ip $IP_ADDR\
 	      --name kickstart \
 	      --network kickstart \
 	      localhost/kickstart
 
-      printf "Done\n"
-   else
-      printf "exists, skipping\n"
+      printf "Done\n" | tee $LOG
+   elif [[ ! $(podman ps | grep -i kickstart) ]]; then
+      podman start kickstart
+      printf "exists, starting\n" | tee $LOG
    fi
 
-   # TODO: DETERMINE IF KS.CFG FILE EXISTS, RENAME IF IT DOES
-   printf "Copying files to kickstart container ..."
+   printf "Copying files to kickstart container ..." | tee $LOG
    
-   podman cp config/ks.cfg kickstart:/var/www/html/download/ks.cfg
+   local uuid=$(uuidgen -r)
+   FILENAME="ks-"${uuid:0:8}".cfg"
+
+   podman cp config/ks.cfg kickstart:/var/www/html/download/$FILENAME
    podman cp www/index.html kickstart:/var/www/html/index.html
 
-   printf "Done\n"
+   printf "Done\n" | tee $LOG
+   printf "\n" | tee $LOG
+   printf "Access the Kickstart config at:\n" | tee $LOG
+   printf "http://$IP_ADDR/download/$FILENAME\n" | tee $LOG
 }
 
 main() {
    # Clean up on EXIT
    trap "pkill -P $$" EXIT
 
-   privilege_check
    parse_args $@
+   privilege_check
    required_pkg_check
    update_config
-  # setup_bridge
    setup_container_net
    create_image
    create_container
